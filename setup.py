75775a43ed3dd15a49f13e75a0d6dfacd1ebba4c
Md. Tareq Mahmood <tareq.py@gmail.com>
2025-06-25 14:26:34 -0500

    Ignore torch cuda mismatch

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def check_cuda_torch_binary_vs_bare_metal(cuda_dir):
     print(raw_output + "from " + cuda_dir + "/bin\n")
 
     if (bare_metal_version != torch_binary_version):
-        raise RuntimeError(
+        print(
             "Cuda extensions are being compiled with a version of Cuda that does "
             "not match the version used to compile Pytorch binaries.  "
             "Pytorch binaries were compiled with Cuda {}.\n".format(torch.version.cuda)
